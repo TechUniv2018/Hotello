a404ab769989b8ff461aa10cb43cf7a4e1b55aff
AbhilashJN <abhilashjn107@gmail.com>
2018-02-11 21:28:00 +0530

    POST /publicLogin : Tested for input validation with string input

--- src/routes/publicLogin.js
+++ src/routes/publicLogin.js
@@ -7,5 +7,15 @@ module.exports = [
     handler: (request, reply) => {
       reply('Login OK');
     },
+    config: {
+      validate: {
+        payload: Joi.object({
+          username: Joi.string().required().min(4)
+            .max(30),
+          password: Joi.string().required().min(8)
+            .max(30),
+        }),
+      },
+    },
   },
 ];
