e4348ce6e5f1795ee0b50ef6fceaf65f44cc8f91
aakash_raj <aakash.raj5@gmail.com>
2018-02-14 17:06:01 +0530

    Tested for jwt token as response and refactored the test file

--- __tests__/server.js
+++ __tests__/server.js
@@ -1,5 +1,6 @@
 const server = require('../src/server');
 const Models = require('../models');
+const jwt = require('jsonwebtoken');
 
 describe('Testing the validation part', () => {
   beforeAll((done) => {
@@ -90,29 +91,36 @@ describe('Testing if database calls are made', () => {
   });
 });
 
-// describe('Testing if the login route for jwt', () => {
-//   // beforeAll((done) => {
-//   //   Models.users.create({
-//   //     username: 'aakash',
-//   //     password: 'aakash12',
-//   //   }).then(done());
-//   // });
-//   // afterAll((done) => {
-//   //   Models.users.destroy({ truncate: true });
-//   //   done();
-//   // });
-//   it('returns login successful for valid form data', (done) => {
-//     const options = {
-//       method: 'POST',
-//       url: '/login',
-//       payload: {
-//         username: 'arpita',
-//         password: 'arpita_jain',
-//       },
-//     };
-//     server.inject(options, (response) => {
-//       expect(response.result).toBe('Invalid credentials');
-//       done();
-//     });
-//   });
-// });
+describe('Testing if login route is returning a jwt token', () => {
+  beforeAll((done) => {
+    Models.users.create({
+      username: 'arpitanew',
+      password: 'arpita_jain',
+    }).then((err) => {
+      console.log('ERROR IS', err);
+      done();
+    });
+  });
+  afterAll((done) => {
+    Models.users.destroy({ truncate: true });
+    done();
+  });
+  it('Passing correct login credentials, Expected output: a JWT token', (done) => {
+    const options = {
+      method: 'POST',
+      url: '/login',
+      payload: {
+        username: 'arpitanew',
+        password: 'arpita_jain',
+      },
+    };
+    server.inject(options, (response) => {
+      const JWT = response.payload;
+      const tokenPayload = jwt.decode(JWT, { complete: true }).payload;
+      expect(tokenPayload).toHaveProperty('iat');
+      expect(tokenPayload).toHaveProperty('exp');
+      expect(tokenPayload).toHaveProperty('name');
+      done();
+    });
+  });
+});
