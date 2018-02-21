67458bc65f969b91a29f2d4907d89a694f073db2
Arpita <arpitajain0811@gmail.com>
2018-02-21 18:17:09 +0530

    resolved merge conflicts after rebase

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,5 +4,6 @@ const updateDetails = require('./userUpdateDetails.js');
 const publicLogin = require('./publicLogin.js');
 const userSignUp = require('./userSignUp');
 const suspendUser = require('./suspendUser');
+const adminUpdateDetails = require('./adminUpdateDetails');
 
-module.exports = [].concat(userSignUp, publicLogin, login, logout, updateDetails, suspendUser);
+module.exports = [].concat(userSignUp, publicLogin, login, logout, updateDetails, suspendUser, adminUpdateDetails);
