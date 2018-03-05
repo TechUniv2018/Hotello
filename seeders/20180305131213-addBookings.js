ca38febd7635ee6002506f95d62c8e0f982ca018
Arpita <arpitajain0811@gmail.com>
2018-03-05 19:12:56 +0530

    added seeder for bookings

---
+++ seeders/20180305131213-addBookings.js
@@ -0,0 +1,42 @@
+
+
+module.exports = {
+  up: (queryInterface, Sequelize) => queryInterface.bulkInsert('bookings', [{
+    bookingid: 'ref3456',
+    email: 'ajaysingh@gmail.com',
+    bookingdate: '2018-02-15',
+    amount: 1720.00,
+    hotelname: 'Royal Orchid',
+    checkin: '2018-02-20',
+    checkout: '2018-02-25',
+    numofguests: 2,
+    numofrooms: 1,
+    city: 'Bangalore',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    bookingid: 'ref3457',
+    email: 'alexander@gmail.com',
+    bookingdate: '2018-05-15',
+    amount: 2120.00,
+    hotelname: 'Grand Orchid',
+    checkin: '2018-02-20',
+    checkout: '2018-02-25',
+    numofguests: 1,
+    numofrooms: 1,
+    city: 'Bangalore',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  }], {}),
+
+  down: (queryInterface, Sequelize) =>
+  /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      */
+    queryInterface.bulkDelete('bookings', null, {}),
+
+};
